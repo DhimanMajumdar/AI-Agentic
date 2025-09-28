3b95929e76c5e5167f9ec9a9b46b1a4d545ccd83
DhimanMajumdar <dhimanmajumdar08233@gmail.com>
2025-09-28 23:18:05 +0530

    fast api and ollama intergration locally

--- ollama-fastapi/server.py
+++ ollama-fastapi/server.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from ollama import Client
 
 app = FastAPI()
+client = Client(host="http://localhost:11434")  # use http not https
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/chat")
+def chat(message: str = Body(..., embed=True, description="The Message")):
+    response = client.chat(
+        model="gemma:2b", messages=[{"role": "user", "content": message}]
+    )
+    return {"response": response["message"]["content"]}
