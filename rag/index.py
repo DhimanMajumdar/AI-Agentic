04ddd5707c017b39aa8a685cbf71f1037659a3c9
DhimanMajumdar <dhimanmajumdar08233@gmail.com>
2025-10-16 21:41:27 +0530

    upto indexing and retrieval done

--- rag/index.py
+++ rag/index.py
@@ -1,5 +1,12 @@
+from dotenv import load_dotenv
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+
+load_dotenv()
+
 
 pdf_path = Path(__file__).parent / "nodejs.pdf"
 
@@ -9,5 +16,19 @@ pdf_path = Path(__file__).parent / "nodejs.pdf"
 loader = PyPDFLoader(file_path=pdf_path)
 docs = loader.load()  # page by page docs
 
+# Split the docs into smaller chunks
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=400)
+chunks = text_splitter.split_documents(documents=docs)
 
-print(docs[12])
+# Vector Embeddings
+embedding_model = OpenAIEmbeddings(model="text-embedding-3-large")
+
+vector_store = QdrantVectorStore.from_documents(
+    documents=chunks,
+    embedding=embedding_model,
+    url="http://localhost:6333",
+    collection_name="learning_rag",
+)
+
+
+print("Indexing of documents done...")
